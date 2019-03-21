af69517c06a5fc6fc8a77ff673fcd8b097b421bc
T.Jurecki <jurecki07@yahoo.com>
2019-03-21 14:47:40 -0400

    objects

---
+++ intermediateJavascript/objects/objectsEx.js
@@ -0,0 +1,42 @@
+let spaceship = {}; // spaceship is an empty object
+
+
+// An object literal with two key-value pairs
+let fasterShip = {
+    'Fuel Type': 'Turbo Fuel',
+    color: 'silver'
+};
+
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+};
+
+let crewCount = spaceship.numCrew;// Returns 5 (Number)
+
+let planetArray = spaceship.flightPath; // Returns an (array) [ 'Venus', 'Mars', 'Saturn' ]
+
+// How a function is setup with Bracket Notation
+let returnAnyProp = (objectName, propName) => objectName[propName];
+
+//using bracket notation
+let isActive = spaceship['Active Mission'];
+console.log(isActive); // Returns True
+
+const spaceship = {type: 'shuttle'};
+spaceship = {type: 'alien'}; // TypeError: Assignment to constant variable.
+spaceship.type = 'alien'; // Changes the value of the type property
+spaceship.speed = 'Mach 5'; // Creates a new key of 'speed' with a value of 'Mach 5'
+
+//Change the color in spaceship property
+spaceship.color = 'New Color';
+
+// Add a new property to spaceship
+spaceship.numEngines = 9;
+
+//Delete a property in spaceship
+delete spaceship['Secret Mission'];
+
